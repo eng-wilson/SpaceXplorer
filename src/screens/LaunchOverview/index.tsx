d5515fe1e1938e941037d37494f8c3d564d89701
Wilson Carvalho <wilson@fuerzastudio.com.br>
2021-07-16 19:16:38 -0300

    chore: styled components added

--- src/screens/LaunchOverview/index.tsx
+++ src/screens/LaunchOverview/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { View } from "react-native";
 
-// import { Container } from './styles';
+import { Container } from "./styles";
 
 const LaunchOverview: React.FC = () => {
-  return <View></View>;
+  return (
+    <Container>
+      <View />
+    </Container>
+  );
 };
 
 export default LaunchOverview;
