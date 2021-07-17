1ef8fa08fa641c2c7727c7cdf1fda41cff79b5ec
Wilson Carvalho <wilson@fuerzastudio.com.br>
2021-07-17 01:00:52 -0300

    style: launch detail screen structure

--- src/screens/LaunchDetail/index.tsx
+++ src/screens/LaunchDetail/index.tsx
@@ -1,11 +1,48 @@
 import React from "react";
+import { TouchableOpacity } from "react-native";
 
-import { SafeContainer, Container } from "./styles";
+import {
+  SafeContainer,
+  ScrollContainer,
+  Background,
+  Container,
+  LaunchName,
+  RocketName,
+  AlignCenterRow,
+  AntDesignIcon,
+  ActionButton,
+  ButtonTitle,
+  ImageContainer,
+  Imagelist,
+} from "./styles";
 
 const LaunchDetail: React.FC = () => {
   return (
     <SafeContainer>
-      <Container />
+      <ScrollContainer bounces={false}>
+        <Background />
+        <Container>
+          <LaunchName>Starlink-15 (v1.0)</LaunchName>
+          <AlignCenterRow>
+            <AntDesignIcon name="rocket1" />
+            <RocketName>Falcon 9</RocketName>
+          </AlignCenterRow>
+
+          <ActionButton>
+            <ButtonTitle>Read article</ButtonTitle>
+          </ActionButton>
+
+          <Imagelist
+            data={[1, 2, 3, 4, 5, 6]}
+            numColumns={2}
+            renderItem={() => (
+              <TouchableOpacity>
+                <ImageContainer />
+              </TouchableOpacity>
+            )}
+          />
+        </Container>
+      </ScrollContainer>
     </SafeContainer>
   );
 };
--- src/screens/LaunchDetail/styles.ts
+++ src/screens/LaunchDetail/styles.ts
@@ -1,12 +1,92 @@
+import { Dimensions } from "react-native";
 import styled from "styled-components/native";
+import { AntDesign } from "@expo/vector-icons";
+
+const screenWidth = Dimensions.get("window").width;
 
 export const SafeContainer = styled.SafeAreaView`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
 `;
 
-export const Container = styled.View`
-  flex: 1;
+export const Background = styled.View`
+  width: 100%;
+  height: 200px;
+  background-color: ${({ theme }) => theme.colors.card};
+`;
 
-  padding: 20px;
+export const ScrollContainer = styled.ScrollView`
+  background-color: ${({ theme }) => theme.colors.card};
+`;
+
+export const Container = styled.View`
+  min-height: 100%;
+
+  padding: 30px 20px;
+
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const LaunchName = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: 24px;
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+export const RocketName = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: 18px;
+  color: ${({ theme }) => theme.colors.primary};
+
+  padding-left: 6px;
+`;
+
+export const AlignCenterRow = styled.View`
+  flex-direction: row;
+
+  align-items: center;
+`;
+
+export const AntDesignIcon = styled(AntDesign)`
+  font-size: 18px;
+
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const ActionButton = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 50px;
+
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  border-radius: 10px;
+
+  margin-top: 20px;
+`;
+
+export const ButtonTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: 18px;
+  color: ${({ theme }) => theme.colors.button_title};
+`;
+
+export const Imagelist = styled.FlatList`
+  align-self: center;
+  margin-top: 20px;
+`;
+
+export const FavoriteButton = styled.TouchableOpacity``;
+
+export const ImageContainer = styled.View`
+  height: ${screenWidth * 0.4};
+  width: ${screenWidth * 0.4};
+  background-color: ${({ theme }) => theme.colors.card};
+
+  border-radius: 10px;
+
+  margin: 10px;
 `;
